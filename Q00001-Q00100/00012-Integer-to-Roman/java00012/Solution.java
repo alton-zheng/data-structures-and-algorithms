659b716460bb0e435ae200be82aa70de5db36fa2
alton <alton_z@outlook.com>
2021-05-14 10:02:07 +0800

    12 #3 solved

---
+++ Q00001-Q00100/00012-Integer-to-Roman/java00012/Solution.java
@@ -0,0 +1,99 @@
+package java00012;
+
+/**
+ * @Author: alton
+ * @Date: Created in 5/14/21 8:54 AM
+ * @Description: 12. Integer to Roman
+ * Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+ * <p>
+ * Symbol       Value
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ * For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+ * <p>
+ * Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+ * <p>
+ * I can be placed before V (5) and X (10) to make 4 and 9.
+ * X can be placed before L (50) and C (100) to make 40 and 90.
+ * C can be placed before D (500) and M (1000) to make 400 and 900.
+ * Given an integer, convert it to a roman numeral.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: num = 3
+ * Output: "III"
+ * Example 2:
+ * <p>
+ * Input: num = 4
+ * Output: "IV"
+ * Example 3:
+ * <p>
+ * Input: num = 9
+ * Output: "IX"
+ * Example 4:
+ * <p>
+ * Input: num = 58
+ * Output: "LVIII"
+ * Explanation: L = 50, V = 5, III = 3.
+ * Example 5:
+ * <p>
+ * Input: num = 1994
+ * Output: "MCMXCIV"
+ * Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= num <= 3999
+ * <p>
+ * Time Complexity: O(1)
+ * Space Complexity: O(1)
+ */
+class Solution2 {
+    int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+    public String intToRoman(int num) {
+        StringBuffer roman = new StringBuffer();
+        for (int i = 0; i < values.length; ++i) {
+            int value = values[i];
+            String symbol = symbols[i];
+            while (num >= value) {
+                num -= value;
+                roman.append(symbol);
+            }
+            if (num == 0) {
+                break;
+            }
+        }
+        return roman.toString();
+    }
+}
+
+class Solution {
+    /**
+     * 因为 num 最大值为 3999
+     * MMM 即可
+     * 百，十，个 都有 9 个有效位，一个无效位（被上一级整除了）
+     */
+    String[] thousands = {"", "M", "MM", "MMM"};
+    String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+    String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    String[] ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+
+    public String intToRoman(int num) {
+        StringBuffer roman = new StringBuffer();
+        roman.append(thousands[num / 1000]);
+        roman.append(hundreds[num % 1000 / 100]);
+        roman.append(tens[num % 100 / 10]);
+        roman.append(ones[num % 10]);
+        return roman.toString();
+    }
+}
