24e7d94a1c61dbdb1e15996018ab1589746d7985
alton <alton_z@outlook.com>
2021-06-12 17:16:34 +0800

    1491. Average Salary Excluding the Minimum and Maximum Salary #10 solved

--- Q01401-Q01500/01491-Average-Salary-Excluding-the-Minimum-and-Maximum-Salary/java01491/m01/Solution.java
+++ Q01401-Q01500/01491-Average-Salary-Excluding-the-Minimum-and-Maximum-Salary/java01491/m01/Solution.java
@@ -7,6 +7,7 @@ package java01491.m01;
  *
  * 1491. Average Salary Excluding the Minimum and Maximum Salary #10
  *
+ *
  * Given an array of unique integers salary where salary[i] is the salary of the employee i.
  *
  * Return the average salary of employees excluding the minimum and maximum salary.
