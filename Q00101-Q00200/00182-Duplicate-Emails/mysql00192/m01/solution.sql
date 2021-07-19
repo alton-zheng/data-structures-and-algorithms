25e81e4a8be01b935eb22955ddb0933e70968b9c
alton <alton_z@outlook.com>
2021-07-19 17:43:20 +0800

    182. Duplicate Emails #222 solved

---
+++ Q00101-Q00200/00182-Duplicate-Emails/mysql00192/m01/solution.sql
@@ -0,0 +1,30 @@
+# Write your MySQL query statement below
+
+-- Create table If Not Exists Person (Id int, Email varchar(255))
+-- Truncate table Person
+-- insert into Person (Id, Email) values ('1', 'a@b.com')
+-- insert into Person (Id, Email) values ('2', 'c@d.com')
+-- insert into Person (Id, Email) values ('3', 'a@b.com')
+
+
+-- Write a SQL query to find all duplicate emails in a table named Person.
+
+-- +----+---------+
+-- | Id | Email   |
+-- +----+---------+
+-- | 1  | a@b.com |
+-- | 2  | c@d.com |
+-- | 3  | a@b.com |
+-- +----+---------+
+-- For example, your query should return the following for the above table:
+--
+-- +---------+
+-- | Email   |
+-- +---------+
+-- | a@b.com |
+-- +---------+
+-- Note: All emails are in lowercase.
+-- Runtime: 259 ms, faster than 98.07% of MySQL online submissions for Duplicate Emails.
+-- Memory Usage: 0B, less than 100.00% of MySQL online submissions for Duplicate Emails.
+select a.Email
+from (select Email, count(1) as cnt from Person group by Email having cnt > 1) a;
