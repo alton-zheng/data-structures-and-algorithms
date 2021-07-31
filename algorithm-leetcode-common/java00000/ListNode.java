430a2b8ca7709dd08ccdc1690c5c5129a540f943
alton <alton_z@outlook.com>
2021-07-31 21:42:18 +0800

    00148-归并迭代.pptx

--- algorithm-leetcode-common/java00000/ListNode.java
+++ algorithm-leetcode-common/java00000/ListNode.java
@@ -3,7 +3,30 @@ package java00000;
 public class ListNode {
     public int val;
     public ListNode next;
-    public ListNode() {}
-    public ListNode(int val) { this.val = val; }
-    public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    public ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder sb = new StringBuilder();
+
+        ListNode temp = next;
+        sb.append("head: ").append(this.val).append(", ");
+        while (temp != null) {
+            sb.append(temp.val).append(", ");
+            temp = temp.next;
+        }
+        return sb.toString();
+    }
 }
