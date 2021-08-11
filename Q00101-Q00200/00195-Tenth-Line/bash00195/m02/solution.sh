65569e423af89b5c6c34cd1a0134426e56c2133b
alton <alton_z@outlook.com>
2021-08-11 13:44:38 +0800

    195. Tenth Line #296 solved

---
+++ Q00101-Q00200/00195-Tenth-Line/bash00195/m02/solution.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+####
+# Runtime: 4 ms, faster than 78.44% of Bash online submissions for Tenth Line.
+# Memory Usage: 3.6 MB, less than 33.50% of Bash online submissions for Tenth Line.
+####
+
+sed -n '10p' file.txt
