6844f7a6eed851d349812677af4e0fab47991fb1
alton <alton_z@outlook.com>
2021-08-24 22:05:52 +0800

    69. Sqrt(x) #338 solved solved

--- Q00001-Q00100/00069-Sqrt-x/java00069/m03/Solution.java
+++ Q00001-Q00100/00069-Sqrt-x/java00069/m03/Solution.java
@@ -6,6 +6,8 @@ package java00069.m03;
  * @Description:
  * Runtime: 1 ms, faster than 100.00% of Java online submissions for Sqrt(x).
  * Memory Usage: 36.3 MB, less than 23.91% of Java online submissions for Sqrt(x).
+ * 个人觉得此方法比较接地气，通俗易懂
+ * 不需要记住牛顿解法的公式或推导过程
  */
 class Solution {
     public int mySqrt(int x) {
--- Q00001-Q00100/00069-Sqrt-x/java00069/m04/Solution.java
+++ Q00001-Q00100/00069-Sqrt-x/java00069/m04/Solution.java
@@ -5,6 +5,30 @@ package java00069.m04;
  * @Date: Created in 2021/8/24 9:14 下午
  * @Description:
  *
+ * Time Complexity: O(logN)
+ * Space Complexity: O(1)
+ * x_(i + 1) = 1/2*(x_i  + C/x_i)
+ * 方法三：牛顿迭代
  */
-public class Solution {
+class Solution {
+    public int mySqrt(int x) {
+
+        if (x == 0) {
+            return 0;
+        }
+
+        // C 值即为需要求平凡根的输入， x0 初始化为输入值
+        double C = x, x0 = x;
+        while (true) {
+            double xi = 0.5 * (x0 + C / x0);
+
+            // 当 2 次 值小于无限大于零值的 1e-7 或 1e-6 时，此时的 x0 即为答案
+            if (Math.abs(x0 - xi) < 1e-7) {
+                break;
+            }
+            x0 = xi;
+        }
+
+        return (int) x0;
+    }
 }
