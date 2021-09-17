d41d1d0482f49a767d6783550a6fa19f67e7646b
alton <alton_z@outlook.com>
2021-09-17 18:29:40 +0800

    class Solution {

---
+++ Q00001-Q00100/00036-Valid-Sudoku/java00036/m01/Solution.java
@@ -0,0 +1,29 @@
+package java00036.m01;
+
+/**
+ * @Author: alton
+ * @Date: Created in 2021/9/17 6:29 下午
+ * @Description:
+ */
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        int[][] rows = new int[9][9];
+        int[][] columns = new int[9][9];
+        int[][][] subboxes = new int[3][3][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+                if (c != '.') {
+                    int index = c - '0' - 1;
+                    rows[i][index]++;
+                    columns[j][index]++;
+                    subboxes[i / 3][j / 3][index]++;
+                    if (rows[i][index] > 1 || columns[j][index] > 1 || subboxes[i / 3][j / 3][index] > 1) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
