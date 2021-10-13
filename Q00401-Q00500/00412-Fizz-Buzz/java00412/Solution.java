7a324a0b65d555debba9c3566d1a9b642fa88f6a
alton <alton_z@outlook.com>
2021-10-13 17:12:59 +0800

    412. Fizz Buzz

--- Q00401-Q00500/00412-Fizz-Buzz/java00412/Solution.java
+++ Q00401-Q00500/00412-Fizz-Buzz/java00412/Solution.java
@@ -7,32 +7,31 @@ import java.util.List;
  * @Author: alton
  * @Date: Created in 5/22/21 8:09 AM
  * @Description:
- * 412. Fizz Buzz #30
- * <p>
- * Given an integer n, return a string array answer (1-indexed) where:
- * <p>
- * answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
- * answer[i] == "Fizz" if i is divisible by 3.
- * answer[i] == "Buzz" if i is divisible by 5.
- * answer[i] == i if non of the above conditions are true.
- * <p>
- * <p>
- * Example 1:
- * <p>
- * Input: n = 3
- * Output: ["1","2","Fizz"]
- * Example 2:
- * <p>
- * Input: n = 5
- * Output: ["1","2","Fizz","4","Buzz"]
- * Example 3:
- * <p>
- * Input: n = 15
- * Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
- * <p>
- * <p>
- * Constraints:
- * <p>
+ * 412. Fizz Buzz
+ * 给你一个整数 n ，找出从 1 到 n 各个整数的 Fizz Buzz 表示，并用字符串数组 answer（下标从 1 开始）返回结果，其中：
+ *
+ * answer[i] == "FizzBuzz" 如果 i 同时是 3 和 5 的倍数。
+ * answer[i] == "Fizz" 如果 i 是 3 的倍数。
+ * answer[i] == "Buzz" 如果 i 是 5 的倍数。
+ * answer[i] == i 如果上述条件全不满足。
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：n = 3
+ * 输出：["1","2","Fizz"]
+ * 示例 2：
+ *
+ * 输入：n = 5
+ * 输出：["1","2","Fizz","4","Buzz"]
+ * 示例 3：
+ *
+ * 输入：n = 15
+ * 输出：["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+ *
+ *
+ * 提示：
+ *
  * 1 <= n <= 104
  * <p>
  * Time Complexity: O(n)
