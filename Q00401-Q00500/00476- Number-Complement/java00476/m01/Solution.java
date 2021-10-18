8946e5262abbfb3a759715890be811fc99aa0440
alton <alton_z@outlook.com>
2021-10-18 08:45:01 +0800

    476. Number Complement #401 solved

--- Q00401-Q00500/00476- Number-Complement/java00476/m01/Solution.java
+++ Q00401-Q00500/00476- Number-Complement/java00476/m01/Solution.java
@@ -5,36 +5,26 @@ package java00476.m01;
  * @Date: Created in 6/21/21 12:41 PM
  * @Description:
  *
- * 476. Number Complement #134
- *
- * Given a positive integer num, output its complement number. The complement strategy is to flip the bits of its binary representation.
+ * 给你一个 正 整数 num ，输出它的补数。补数是对该数的二进制表示取反。
  *
  *
+ * 示例 1：
  *
- * Example 1:
+ * 输入：num = 5
+ * 输出：2
+ * 解释：5 的二进制表示为 101（没有前导零位），其补数为 010。所以你需要输出 2 。
+ * 示例 2：
  *
- * Input: num = 5
- * Output: 2
- * Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. So you need to output 2.
- * Example 2:
+ * 输入：num = 1
+ * 输出：0
+ * 解释：1 的二进制表示为 1（没有前导零位），其补数为 0。所以你需要输出 0 。
  *
- * Input: num = 1
- * Output: 0
- * Explanation: The binary representation of 1 is 1 (no leading zero bits), and its complement is 0. So you need to output 0.
+ * 提示：
  *
- *
- * Constraints:
- *
- * The given integer num is guaranteed to fit within the range of a 32-bit signed integer.
+ * 给定的整数 num 保证在 32 位带符号整数的范围内。
  * num >= 1
- * You could assume no leading zero bit in the integer’s binary representation.
- * This question is the same as 1009: https://leetcode.com/problems/complement-of-base-10-integer/
- *
- * Time Complexity : O(L)
- * Space Complexity : O(L)
- *
- * Runtime: 1 ms, faster than 29.81% of Java online submissions for Number Complement.
- * Memory Usage: 35.8 MB, less than 61.23% of Java online submissions for Number Complement
+ * 你可以假定二进制数不包含前导零位。
+ * 本题与 1009 https://leetcode-cn.com/problems/complement-of-base-10-integer/ 相同
  *
  */
 class Solution {
@@ -51,5 +41,6 @@ class Solution {
         }
 
         return Integer.parseInt(String.valueOf(res), 2);
+
     }
 }
