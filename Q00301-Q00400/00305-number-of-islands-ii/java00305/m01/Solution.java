80c7bd82eea69acb454719701b04c44b91f32f44
alton <alton_z@outlook.com>
2022-09-03 09:27:00 +0800

    200. Number of Islands #458

---
+++ Q00301-Q00400/00305-number-of-islands-ii/java00305/m01/Solution.java
@@ -0,0 +1,9 @@
+package java00305.m01;/**
+ * @Description: 
+ * @Author: alton_z
+ * @Create Date Time: 2022-09-03 09:04
+ * @Update Date Time: 2022-09-03 09:04
+ * @Updated-By: alton_z
+ * @Update-Location: 
+ */public class Solution {
+}
---
+++ Q00901-Q01000/00952-Largest-Component-Size-by-Common-Factor/java00952/m01/Solution.java
@@ -0,0 +1,9 @@
+package java00952.m01;/**
+ * @Description: 
+ * @Author: alton_z
+ * @Create Date Time: 2022-09-03 09:07
+ * @Update Date Time: 2022-09-03 09:07
+ * @Updated-By: alton_z
+ * @Update-Location: 
+ */public class Solution {
+}
