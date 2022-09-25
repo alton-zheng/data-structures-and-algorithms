ffb1bdde38dec0a5f1278e738f5085dbcd8e9687
alton <alton_z@outlook.com>
2022-09-25 09:17:11 +0800

    788. Rotated Digits #486

--- Q00701-Q00800/00788-Rotated-Digits/java00788/m01/Solution.java
+++ Q00701-Q00800/00788-Rotated-Digits/java00788/m01/Solution.java
@@ -1,33 +1,32 @@
 package java00788.m01;
 
 /**
- * @Author: alton
- * @Date: Created in 7/7/21 9:28 PM
- * @Description:
- *
- * 788. Rotated Digits
- * x is a good number if after rotating each digit individually by 180 degrees, we get a valid number that is different from x. Each digit must be rotated - we cannot choose to leave it alone.
- *
- * A number is valid if each digit remains a digit after rotation. 0, 1, and 8 rotate to themselves; 2 and 5 rotate to each other (on this case they are rotated in a different direction, in other words 2 or 5 gets mirrored); 6 and 9 rotate to each other, and the rest of the numbers do not rotate to any other number and become invalid.
- *
- * Now given a positive number n, how many numbers x from 1 to n are good?
- *
- * Example:
- * Input: 10
- * Output: 4
- * Explanation:
- * There are four good numbers in the range [1, 10] : 2, 5, 6, 9.
- * Note that 1 and 10 are not good numbers, since they remain unchanged after rotating.
- * Note:
- *
- * n will be in range [1, 10000].
- *
- * Time Complexity : O()
- * Space Complexity : O()
- *
+ * 枚举方法
  */
-/*class Solution.cpp {
+class Solution {
+    static int[] help = {0, 0, 1, -1, -1, 1, 1, -1, 0, 1};
     public int rotatedDigits(int n) {
+        int res = 0;
+        for (int i = 1; i <= n; ++i) {
+            String cur = String.valueOf(i);
+            boolean condition1 = true, condition2 = false;
+            for (int j = 0; j < cur.length(); ++j) {
+                char ch = cur.charAt(j);
+                if (help[ch - '0'] == -1) {
+                    condition1 = false;
+                    break;
+                }
 
+                if (help[ch - '0'] == 1) {
+                    condition2 = true;
+                }
+            }
+
+            if (condition1 && condition2) {
+                res++;
+            }
+        }
+        return res;
     }
-}*/
+}
+
